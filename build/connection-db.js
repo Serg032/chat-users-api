c08c48712b1b2aa7115a764954484c09883a5cd9
Sergio Radigales <sergioradigales@gmail.com>
2024-07-03 19:47:00 +0200

    build

--- build/connection-db.js
+++ build/connection-db.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const sequelize = new sequelize_1.Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASS, {
-    host: "localhost",
+    host: process.env.DB_HOST,
     dialect: "postgres",
 });
 exports.default = sequelize;
